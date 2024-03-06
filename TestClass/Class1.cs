f5f099bce80d2ead8ab8a922b7979af10c302668
lovep <lovep@LOVEPREET>
2024-03-06 14:33:28 -0500

    Test for a Valid Isosceles Triangle

--- TestClass/Class1.cs
+++ TestClass/Class1.cs
@@ -13,19 +13,66 @@ namespace TestClass
         [Test]
         public void TestEquilateralTriangle_Valid_ReturnsEquilateral()
         {
-            // Arrange a Test
+            // Arrange
             int firstSide = 9;
             int secondSide = 9;
             int thirdSide = 9;
+            string expected = ("An equilateral triangle is formed");
 
-            string excepted = ("An equilateral triangle is formed");
-
-            // Perform a Test
+            // Act
 
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
 
-            // Assert a Test
-            Assert.That(actual, Is.EqualTo(excepted));
+            // Assert
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        [Test]
+        public void TestIsoscelesTriangle_Valid1_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 9;
+            int secondSide = 9;
+            int thirdSide = 6;
+            string expected = "An isosceles triangle is formed";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        [Test]
+        public void TestIsoscelesTriangle_Valid2_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 6;
+            int secondSide = 9;
+            int thirdSide = 9;
+            string expected = ("An isosceles triangle is formed");
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        [Test]
+        public void TestIsoscelesTriangle_Valid3_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 9;
+            int secondSide = 6;
+            int thirdSide = 9;
+
+            string expected = ("An isosceles triangle is formed");
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
