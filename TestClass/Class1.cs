c7b390d19bfa62803e30ccd655de2f6f77c71fff
lovep <lovep@LOVEPREET>
2024-03-06 14:18:45 -0500

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TestClass
+{
+    public class TestTriangle
+    {
+        [Test]
+        public void TestEquilateralTriangle_Valid_ReturnsEquilateral()
+        {
+            // Arrange a Test
+            int firstSide = 9;
+            int secondSide = 9;
+            int thirdSide = 9;
+
+            string excepted = ("An equilateral triangle is formed");
+
+            // Perform a Test
+
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert a Test
+            Assert.That(actual, Is.EqualTo(excepted));
+        }
+    }
+}
